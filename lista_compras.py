11ba2ee069c59f86b23ad4eef069ddfa89828219
crmarselha <carlosrafaelmarselha@gmail.com>
2026-02-12 09:16:52 -0300

    Implement shopping list functionality in lista_compras.py

---
+++ lista_compras.py
@@ -0,0 +1,32 @@
+import os
+
+lista = []
+while True:
+  print('Selecione uma opção:')
+  opção = input('[i]nserir, [a]pagar, [l]istar: ')
+
+  if opção == 'i':
+      os.system('cls')
+      valor = input('Valor: ')
+      lista.append(valor)
+  elif opção == 'a':
+      indice_str = input('Escolha um índice para apagar: ')
+      try:
+          indice = int(indice_str)
+          del lista[indice]
+      except ValueError:
+          print('Por favor, digite um número inteiro!')
+      except IndexError:
+          print('Índice não existe na lista!')
+      except Exception:
+          print('Erro desconhecido!')
+  elif opção == 'l':
+      os.system('cls')
+      
+      if len(lista) == 0:
+          print('Lista vazia!')
+      
+      for i, valor in enumerate(lista):
+          print(i, valor)
+  else:
+      print('Por favor, escolha i, a ou l.')
